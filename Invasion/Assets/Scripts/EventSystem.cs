a4649d4a83a05e43b66ee2a23875f65246528ec2
MIRON <1111111111aleksey46@gmail.com>
2022-09-12 22:48:28 +0300

    add sync animation

--- Invasion/Assets/Scripts/EventSystem.cs
+++ Invasion/Assets/Scripts/EventSystem.cs
@@ -1,18 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using Mirror;
 
-public class EventSystem : MonoBehaviour
+public class EventSystem : NetworkBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public static EventSystem singleton { get; private set; }
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        singleton = this;
     }
 }
--- Invasion/Assets/Scripts/Player/PlayerController.cs
+++ Invasion/Assets/Scripts/Player/PlayerController.cs
@@ -5,17 +5,24 @@ public class PlayerController : NetworkBehaviour
 {
     [SerializeField]
     public float _speed;
+    public Animator _animator;
 
     private void Start()
     {
-        _speed = 5;
+        _speed = 10;
+        _animator = GetComponent<Animator>();
     }
     private void Update()
     {
-        float x = Input.GetAxis("Horizontal");
-        float y = Input.GetAxis("Vertical");
-        Vector3 move = new Vector3(x * _speed * Time.deltaTime, y * _speed * Time.deltaTime, 0);
-        transform.position += move;
+        if (!isLocalPlayer) return;
 
+        float x = Input.GetAxis("Horizontal") * _speed;
+        float y = Input.GetAxis("Vertical") * _speed;
+
+        _animator.SetFloat("SpeedY", y);
+        _animator.SetFloat("SpeedX", x);
+
+        Vector3 move = new Vector3(x * Time.deltaTime, y * Time.deltaTime, 0);
+        transform.position += move;
     }
 }
